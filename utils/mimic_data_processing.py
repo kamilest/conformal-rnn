f7c637f7dfb2095de3f753e1be7ed3adae51db3c
kamilest <stankeviciute.kamile@gmail.com>
2021-05-23 15:46:35 +0100

    MIMIC data processing bug fixes/refactoring.

--- utils/mimic_data_processing.py
+++ utils/mimic_data_processing.py
@@ -24,7 +24,7 @@ class MIMICDataset(torch.utils.data.Dataset):
         return self.X[idx], self.Y[idx], self.sequence_lengths[idx]
 
 
-def process_MIMIC_data(horizon=2, feature='wbchigh'):
+def process_mimic_data(horizon=2, feature='wbchigh'):
     feature_names = ['temphigh', 'heartratehigh', 'sysbplow', 'diasbplow',
                      'meanbplow', 'spo2high',
                      'fio2high', 'respratelow', 'glucoselow', 'bicarbonatehigh',
@@ -51,15 +51,28 @@ def process_MIMIC_data(horizon=2, feature='wbchigh'):
 
     X_ = []
     Y_ = []
-    for k in np.where(L > 5):
+    L_ = []
+    for k in np.where(L > 4)[0]:
         X_.append(Y_scaled[k, :L[k] - horizon])
         Y_.append(Y[k, L[k] - horizon:L[k]])
+        L_.append(L[k])
 
-    return X_, Y_, L
+    return X_, Y_, L_
 
 
-def split_MIMIC_data(n_train=2800, n_calibration=1823, n_test=500,
-                     conformal=True, feature='wbchigh'):
+def get_mimic_dataset(X, Y, L, idx):
+    X_ = torch.nn.utils.rnn.pad_sequence([torch.FloatTensor(X[i]).reshape(-1, 1)
+                                          for i in idx],
+                                         batch_first=True)
+    Y_ = torch.nn.utils.rnn.pad_sequence([torch.FloatTensor(Y[i]).reshape(-1, 1)
+                                          for i in idx],
+                                         batch_first=True).float()
+
+    return MIMICDataset(X_, Y_, [L[i] for i in idx])
+
+
+def get_mimic_splits(n_train=2000, n_calibration=1823, n_test=500,
+                     conformal=True, feature='wbchigh', horizon=2):
     perm = np.random.RandomState(seed=0).permutation(n_train + n_calibration +
                                                      n_test)
     train_idx = perm[:n_train]
@@ -67,18 +80,18 @@ def split_MIMIC_data(n_train=2800, n_calibration=1823, n_test=500,
     train_calibration_idx = perm[:n_train + n_calibration]
     test_idx = perm[n_train + n_calibration:]
 
-    X_, Y_, L = process_MIMIC_data(feature=feature)
+    X_, Y_, L = process_mimic_data(feature=feature, horizon=horizon)
     assert n_train + n_calibration + n_test == len(X_)
 
     if conformal:
-        train_dataset = MIMICDataset(X_[train_idx], Y_[train_idx], L[train_idx])
-        calibration_dataset = MIMICDataset(X_[calibration_idx],
-                                           Y_[calibration_idx],
-                                           L[calibration_idx])
-        test_dataset = MIMICDataset(X_[test_idx], Y_[test_idx], L[test_idx])
+        train_dataset = get_mimic_dataset(X_, Y_, L, train_idx)
+        calibration_dataset = get_mimic_dataset(X_, Y_, L, calibration_idx)
+        test_dataset = get_mimic_dataset(X_, Y_, L, test_idx)
+
     else:
-        train_dataset = X_[train_calibration_idx], Y_[train_calibration_idx]
+        train_dataset = [X_[i] for i in train_calibration_idx], \
+                        [Y_[i] for i in train_calibration_idx],
         calibration_dataset = None
-        test_dataset = X_[test_idx], Y_[test_idx]
+        test_dataset = [X_[i] for i in test_idx], [Y_[i] for i in test_idx]
 
     return train_dataset, calibration_dataset, test_dataset
