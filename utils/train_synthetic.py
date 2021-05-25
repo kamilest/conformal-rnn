411740e3564a130093fc0e6dc10ecfabef96ea93
kamilest <stankeviciute.kamile@gmail.com>
2021-05-25 23:38:25 +0100

    Update synthetic data result reporting.

--- utils/train_synthetic.py
+++ utils/train_synthetic.py
@@ -26,7 +26,6 @@ def train_conformal_forecaster(n_train_samples=1000,
                                embedding_size=20,
                                coverage=0.9,
                                lr=0.01):
-
     if noise_profile is None:
         noise_profile = [0.1 * k for k in range(seq_len + horizon)]
 
@@ -74,14 +73,25 @@ def train_conformal_forecaster(n_train_samples=1000,
     model.fit(train_dataset, calibration_dataset, epochs=epochs, lr=lr,
               batch_size=batch_size)
 
-    coverages, intervals = model.evaluate_coverage(test_dataset)
-    mean_coverage = torch.mean(coverages.float(), dim=0).item()
-    interval_widths = (intervals[:, 1] - intervals[:, 0]).squeeze().mean(
-        dim=0).tolist()
-
-    results = {'coverages': coverages,
-               'intervals': intervals,
-               'mean_coverage': mean_coverage,
-               'interval_widths': interval_widths}
+    independent_coverages, joint_coverages, intervals = \
+        model.evaluate_coverage(test_dataset)
+    mean_independent_coverage = torch.mean(independent_coverages.float(), dim=0)
+    mean_joint_coverage = torch.mean(joint_coverages.float(), dim=0).item()
+    interval_widths = (intervals[:, 1] - intervals[:, 0]).squeeze()
+    point_predictions, errors = \
+        model.get_point_predictions_and_errors(test_dataset)
+    results = {'Point predictions': point_predictions,
+               'Errors': errors,
+               'Independent coverage indicators':
+                   independent_coverages.squeeze(),
+               'Joint coverage indicators':
+                   joint_coverages.squeeze(),
+               'Upper limit': intervals[:, 1],
+               'Lower limit': intervals[:, 0],
+               'Mean independent coverage':
+                   mean_independent_coverage.squeeze(),
+               'Mean joint coverage': mean_joint_coverage,
+               'Confidence interval widths': interval_widths,
+               'Mean confidence interval widths': interval_widths.mean(dim=0)}
 
     return results
