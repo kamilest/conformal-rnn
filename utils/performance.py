dba8eb67c2d3352a86df7da861c41286942150b4
kamilest <stankeviciute.kamile@gmail.com>
2021-08-08 23:01:45 +0300

    Remove redundant result collection methods.

--- utils/performance.py
+++ utils/performance.py
@@ -10,7 +10,6 @@ from matplotlib import pyplot as plt
 from models.bjrnn import RNN_uncertainty_wrapper
 from models.dprnn import DPRNN
 from models.qrnn import QRNN
-from models.rnn import RNN
 from utils.data_processing_synthetic import *
 
 
@@ -46,22 +45,6 @@ def evaluate_cornn_performance(model, test_dataset, correct_conformal=True,
                    interval_widths.mean(dim=0)}
 
     return results
-
-
-def plot_1D_uncertainty(results, Y_test, data_index):
-    plt.fill_between(list(range(len(results["Lower limit"][data_index]))),
-                     results["Lower limit"][data_index].reshape(-1, ),
-                     results["Upper limit"][data_index].reshape(-1, ),
-                     color="r", alpha=0.25)
-
-    plt.plot(results["Lower limit"][data_index], linestyle=":", linewidth=3,
-             color="r")
-    plt.plot(results["Upper limit"][data_index], linestyle=":", linewidth=3,
-             color="r")
-
-    plt.plot(Y_test[data_index], linestyle="--", linewidth=2, color="black")
-    plt.plot(results["Point predictions"][data_index], linewidth=3, color="r",
-             Marker="o")
 
 
 def get_bjrnn_coverage(intervals_, target, coverage_mode='joint'):
@@ -121,49 +104,3 @@ def evaluate_performance(model, X_test, Y_test, coverage=.9):
         results["Joint coverage indicators"])
 
     return results
-
-
-def collect_synthetic_results(noise_vars, params, coverage=0.9, seq_len=5,
-                              n_train_seq=1000, n_test_seq=1000):
-    # noise_profs     = [noise_vars[k] * np.ones(seq_len) for k in range(len(
-    # noise_vars))]
-    noise_profs = noise_vars * np.ones(seq_len)
-
-    result_dict = dict({"BJRNN": [], "QRNN": [], "DPRNN": []})
-
-    model_type = [RNN, QRNN, DPRNN]
-    model_names = ["BJRNN", "QRNN", "DPRNN"]
-
-    for u in range(len(model_type)):
-
-        X, Y = create_autoregressive_data(n_samples=n_train_seq,
-                                          noise_profile=noise_profs,
-                                          seq_len=seq_len,
-                                          mode="time-dependent")
-
-        RNN_model = model_type[u](**params)
-
-        print("Training model " + model_names[
-            u] + " with aleatoric noise variance %.4f and %d training "
-                 "sequences" % (
-                  noise_vars, n_train_seq))
-
-        RNN_model.fit(X, Y)
-
-        if type(RNN_model) is RNN:
-
-            RNN_model_ = RNN_uncertainty_wrapper(RNN_model)
-
-        else:
-
-            RNN_model_ = RNN_model
-
-        X_test, Y_test = create_autoregressive_data(n_samples=n_test_seq,
-                                                    noise_profile=noise_profs,
-                                                    seq_len=seq_len,
-                                                    mode="time-dependent")
-
-        result_dict[model_names[u]].append(
-            evaluate_performance(RNN_model_, X_test, Y_test, coverage=coverage))
-
-    return result_dict
