109905156e2c7ab894987c6ac80cc15cf4df93f1
Evgeny Saveliev <e.s.saveliev@gmail.com>
2021-12-01 23:08:10 +0000

    Add tensor to array conversion

--- utils/performance.py
+++ utils/performance.py
@@ -65,7 +65,7 @@ def evaluate_performance(model, X_test, Y_test, coverage=0.9):
     upper = np.array(y_u_approx).squeeze()
     lower = np.array(y_l_approx).squeeze()
     pred = np.array(y_pred).squeeze()
-    target = np.array([t for t in Y_test]).squeeze()
+    target = np.array([t.numpy() if isinstance(t, torch.Tensor) else t for t in Y_test]).squeeze()
 
     results["Point predictions"] = np.array(y_pred)
     results["Upper limit"] = np.array(y_u_approx)
